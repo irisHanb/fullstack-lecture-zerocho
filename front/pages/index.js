a38e3bc7a97f5f0c5ba9372d8a0bcfcf7ef3a215
irisHanb <fortrulymylife@gmail.com>
2019-07-08 15:54:45 +0900

    setting front base

---
+++ front/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Home = () => {
+  return (
+    <>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <div>Hello, Next!</div>
+    </>
+  );
+};
+
+export default Home;
